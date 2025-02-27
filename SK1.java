4dc972440588724aa28ea5b0a7bc6364451a4f03
M. Raka Fadillah <121095752+rakafdil@users.noreply.github.com>
2025-02-27 15:34:18 +0700

    Update SK1.java

--- SK1.java
+++ SK1.java
@@ -35,7 +35,7 @@ public class SK1{
             if (pilihan == 3) {
                 System.out.println("Masukkan data yang ingin dicari posisinya: ");
                 data = scan.nextInt();
-                findAllposisiData(A, data, n);
+                findAllPosData(A, data, n);
                 System.out.println("");
             }
             if (pilihan == 4) {
@@ -63,23 +63,58 @@ public class SK1{
             System.out.println("Elemen ke " + i + " : ");
             A[i] = scan.nextInt();
         }
-        scan.close();
     
     }
 
-    public static void findMin(int[] a, int n) {
+    public static void findMin(int[] A, int n) {
         int i, minValue;
 
-        minValue = a[0];
+        minValue = A[0];
         for (i = 1; i <= n - 1; i++) {
-            if (a[i] < minValue) {
-                minValue = a[i];
+            if (A[i] < minValue) {
+                minValue = A[i];
             }
         }
         for (i = 0; i <= n - 1; i++) {
-            if (a[i] == minValue) {
+            if (A[i] == minValue) {
                 System.out.println("Nilai terkecil: " + minValue + " ditemukan pada posisi: " + i);
             }
+        }
+    }
+    
+    public static void cetakArray(int[] A, int n) {
+        int i;
+        for (i =0; i<=n-1; i++){
+            System.out.println(A[i]);
+        }
+    }
+    
+    public static void findPosGenap(int[] A, int n) {
+        boolean even = false;
+        int i;
+        
+        for (i=0; i<=n-1;i++){
+            if (A[i]%2==0){
+                System.out.println("Posisi data genap ditemukan di indeks ke "+ i);
+                even = true;
+            }
+        }
+        if (even==false){
+            System.out.println("Tidak ditemukan");
+        }
+    }
+    
+    public static void findAllPosData(int[] A,int data, int n) {
+        boolean found = false;
+        int i;
+        for(i=0; i<=n-1; i++){
+            if (A[i]==data) {
+                System.out.println("Posisi data "+ data+ " ditemukan di indeks ke "+i);
+                found = true;
+            }
+        }
+        if (found==false){
+            System.out.println("Tidak ditemukan");
         }
     }
 }
